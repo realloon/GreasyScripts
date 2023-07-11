ca7f720236a4cc9b8114852f8b995fe8699168c9
真真爱编程 <30503085+realloon@users.noreply.github.com>
2023-07-11 23:25:00 +0800

    consistency in code style

--- scripts/export-answers-By-Chaoxing.js
+++ scripts/export-answers-By-Chaoxing.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Batch export answers By Chaoxing
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  Export all the answers on the Chaoxing homework page as a txt file.
 // @author       realloon
 // @match        *://mooc1.chaoxing.com/mooc2/work/*
@@ -9,45 +9,46 @@
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-(() => {
-    'use strict';
-
+;(() => {
+    'use strict'
+  
     // Your code here...
     function saveFile(fileName = 'example.txt', fileContent = 'Hello, world') {
-        const blob = new Blob([fileContent], { type: 'text/plain;charset=utf-8' });
-
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = fileName;
-
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+      const blob = new Blob([fileContent], { type: 'text/plain;charset=utf-8' })
+  
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(blob)
+      link.download = fileName
+  
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
     }
-
+  
     function processContent(content) {
-        content = content && String(content.substr(1))
-        content = content ? '\n' + content : ''
-        return content
+      content = content && String(content.substr(1))
+      content = content ? '\n' + content : ''
+      return content
     }
-
-    GM_registerMenuCommand("导出全部答案", ()=>{
-        const markKey = document.querySelectorAll('.mark_key.clearfix')
-
-        const answer = Array.from(markKey).map((mark, index) => {
-            const [optionEl, contentEl] = mark.querySelectorAll(
-                '.colorGreen.marginRight40'
-            )
-
-            const option = optionEl.innerText
-            const content = processContent(contentEl && contentEl.innerText)
-
-            return `${index + 1}. ${option}${content}\n`
-        })
-
-        const markTitleEl = document.querySelector('.mark_title')
-        const markTitle = markTitleEl.innerText
-
-        saveFile(markTitle, answer.join(''))
+  
+    GM_registerMenuCommand('导出全部答案', () => {
+      const markKey = document.querySelectorAll('.mark_key.clearfix')
+  
+      const answer = Array.from(markKey).map((mark, index) => {
+        const [optionEl, contentEl] = mark.querySelectorAll(
+          '.colorGreen.marginRight40'
+        )
+  
+        const option = optionEl.innerText
+        const content = processContent(contentEl && contentEl.innerText)
+  
+        return `${index + 1}. ${option}${content}\n`
+      })
+  
+      const markTitleEl = document.querySelector('.mark_title')
+      const markTitle = markTitleEl.innerText
+  
+      saveFile(markTitle, answer.join(''))
     })
-})();
+  })()
+  
